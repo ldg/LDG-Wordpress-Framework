4289c6cd9f13b08f0c90d0bb3b4355648d5c56e9
ldg <scottm@lostdoggrafix.com>
2014-09-05 16:27:57 -0400

    added support for large thumbnails, limited word count on excerpts, customization of the login screen and editor style sheet to reflect the theme's style in the visual editor

--- functions.php
+++ functions.php
@@ -296,9 +296,82 @@ if( ! function_exists( 'lost_scripts' ) ) {
 	add_action( 'wp_enqueue_scripts', 'lost_scripts' );
 }
 
+/**
+* -----------------------------------------------------------------------------------
+* 11.0 - add support for 250x250 thumbs
+* -----------------------------------------------------------------------------------
+*/
+function my_image_sizes($sizes) {
+    $addsizes = array(
+        "square-large" => __( "Large square image")
+    );
+    $newsizes = array_merge($sizes, $addsizes);
+    return $newsizes;
+}
 
+/**
+* -----------------------------------------------------------------------------------
+* 12.0 - limit the word count in excerpts
+* -----------------------------------------------------------------------------------
+*/
+function string_limit_words($string, $word_limit)
+{
+  $words = explode(' ', $string, ($word_limit + 1));
+  if(count($words) > $word_limit)
+  array_pop($words);
+  return implode(' ', $words);
+}
 
+/**
+* -----------------------------------------------------------------------------------
+* 13.0 - Customize the Wordpress login
+* -----------------------------------------------------------------------------------
+*/
+function al_login_log() { 
+		$logo = get_stylesheet_directory_uri() . '/assets/img/login_logo.png';
+		//logo size
+		$logo_size = getimagesize( $logo );
+	?>
+    <style type="text/css">
+    	body.login { background-color: #f4eedc; color: #847E6E;}
+    	body.login div#login form#loginform { 
+    		border-radius: 8px; 
+    		background-color: #E9DEBE; 
+    		box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.5); 
+    		padding-left: 30px;
+    		padding-right: 30px;
+    		width: 250px;
+    	}
+    	body.login div#login form#loginform input {
+    		border-radius: 8px;
+    		border: 1px solid #E9DEBE;
+    		padding: 8px;
+    	}
+        body.login div#login h1 a {
+            background: transparent url( <?php echo $logo; ?> ) 0 0 no-repeat;;
+            width: <?php echo $logo_size[0] ?>px;
+			height: <?php echo $logo_size[1] ?>px;
+            padding-bottom: 0px;
+        }
+        body.login div#login form#loginform p.submit input#wp-submit {
+        	padding: 0px 12px;
+        }
+    </style>
+<?php }
+add_action( 'login_enqueue_scripts', 'al_login_log' );
 
+/**
+* -----------------------------------------------------------------------------------
+* 14.0 - Add Editor CSS
+* -----------------------------------------------------------------------------------
+*/
+function al_add_editor_styles() {
+ // **** add google webfont links ******
+ // $font_url = urlencode( 'http://fonts.googleapis.com/css?family=Lato:100,300,400|Patua+One' );
+ // add_editor_style( $font_url );
+    add_editor_style( 'custom-editor-style.css' );
+}
+add_action( 'after_setup_theme', 'al_add_editor_styles' );
 
 
 
